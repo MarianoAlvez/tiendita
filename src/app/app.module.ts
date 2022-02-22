bd8a7e0b52db43d223207d6cdfa1cbc4004e9662
marianoAlvez <alvez_jmariano@hotmail.com>
2022-02-22 19:33:14 -0300

    Modulos, componentes, interfaces creadas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CatalogComponent } from './caalog/catalog.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CatalogComponent,
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
